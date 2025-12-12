3b6651a894fe5645280d71b8547f97e3f7919653
vperim <vtperim@gmail.com>
2025-12-12 16:30:59 -0300

    Renomeado SwaggerExampleOperationFilter para evitar exclusão no Docker

--- BoletoNetCore.Server/Extensions/ServiceCollectionExtensions.cs
+++ BoletoNetCore.Server/Extensions/ServiceCollectionExtensions.cs
@@ -61,7 +61,7 @@ public static class ServiceCollectionExtensions
                 });
 
             c.OperationFilter<SwaggerVersioningOperationFilter>();
-            c.OperationFilter<SwaggerExampleOperationFilter>();
+            c.OperationFilter<SwaggerDefaultRequestFilter>();
         });
     }
 }
